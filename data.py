3c447c5bc53e4af88bd273115de1bcdba3a89553
Megan <megan@Megans-MacBook-Pro-9.local>
2023-03-10 07:00:31 -0500

    adding data.py for olivia to main

---
+++ data.py
@@ -0,0 +1,47 @@
+import csv
+import pandas as pd
+import re
+from datetime import date
+from datetime import datetime
+import json
+import boto3  # pip install boto3
+
+# Let's use Amazon S3
+s3 = boto3.resource("s3")
+client = boto3.client("s3")
+
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
+print('\n')
+
+
+#now get only the profile-list: the list of nicknames that exist 
+                            #bucket name    #file name
+content_object = s3.Object('profiles-list','profilenames.json')
+file_content = content_object.get()['Body'].read().decode('utf-8')
+json_content = json.loads(file_content)
+profilesList = json_content['profiles']
+
+
+#for each nickname, you can get the individual profile information associated with the nickname
+#the nickname will also have the uploaded csv file - watch history that we can get
+for profile in profilesList: 
+    #print(profile)
+
+    #individual profile information - key is: nickname + userprofile.json
+    profileObject = s3.Object('added-new-person', profile + 'userprofile.json')
+    profile_content = profileObject.get()['Body'].read().decode('utf-8')
+    profilejson_content = json.loads(profile_content)
+    print('nickname: ' + profilejson_content['nickname'])
+    print('username: ' + profilejson_content['username'])
+    print('password: ' + profilejson_content['password'])
+    print('icon: ' + profilejson_content['icon'])
+    
+    #individual watch history csv file - key is nickname + csvfile.csv
+    #note you have to use the s3 client now, not the resource
+    obj = client.get_object(Bucket='added-new-person', Key= profile + 'csvfile.csv')
+    csvfilecontent = pd.read_csv(obj['Body'])
+    print(csvfilecontent)
+    print('\n')
+    
