157567fa47302ea57598ec1c2e115d1691a03cb3
Megan Andree <mra117@case.edu>
2023-04-11 20:50:11 -0400

    update results

--- TestProject/app.js
+++ TestProject/app.js
@@ -81,7 +81,7 @@ app.post('/algorithm', async function(req, res, next) {
   for(var i = 0; i < resultsObjects.length; i++){
     var item1 = await getResultFile(resultsObjects[i]);
     console.log(item1);
-    tosend.push([item1['title'], item1['description'], item1['poster']]);
+    tosend.push([item1['title'], item1['description'], item1['poster'], item1['releaseDate']]);
   }
   console.log(tosend)
   res.render('results', { results: tosend, title: 'Express' });
